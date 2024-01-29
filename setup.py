e6f5a8a40db79db4c6c76f97343b8f75800c33e3
louis-mtyr <94978517+louis-mtyr@users.noreply.github.com>
2024-01-29 14:29:24 +0100

    setup + board creation

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='game_of_life-louis_martyr-CDOF4',
+ version='1.0',
+ author='Louis MARTYR',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
